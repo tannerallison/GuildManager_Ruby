bedb266cae1b123fc79579c97fdf30a2c09be2eb
Tanner <tanner@atallison.com>
2023-05-12 23:26:02 -0500

    Adding Devise auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_12_183627) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_13_042545) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "minions", force: :cascade do |t|
+    t.string "name"
+    t.integer "status"
+    t.bigint "player_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["player_id"], name: "index_minions_on_player_id"
+  end
 
   create_table "players", force: :cascade do |t|
     t.string "username"
@@ -20,6 +29,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_12_183627) do
     t.string "api_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_players_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_players_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "minions", "players"
 end
